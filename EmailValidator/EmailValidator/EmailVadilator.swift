b9954c7853e1bf6cdc265161bb95b65642c6ef27
Suneel Apprikart <developer@apprikart.com>
2022-08-24 12:16:18 +0530

    Add framework

---
+++ EmailValidator/EmailValidator/EmailVadilator.swift
@@ -0,0 +1,15 @@
+//
+//  EmailVadilator.swift
+//  EmailValidator
+//
+//  Created by Suneel on 24/08/22.
+//
+
+import Foundation
+
+public class EmailValidator {
+    
+    public class func testEmail() {
+        print("Email validator...")
+    }
+}
---
+++ EmailValidator/EmailValidator/EmailValidator.h
@@ -0,0 +1,18 @@
+//
+//  EmailValidator.h
+//  EmailValidator
+//
+//  Created by Suneel on 24/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailValidator.
+FOUNDATION_EXPORT double EmailValidatorVersionNumber;
+
+//! Project version string for EmailValidator.
+FOUNDATION_EXPORT const unsigned char EmailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailValidator/PublicHeader.h>
+
+
